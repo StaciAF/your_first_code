f2365b36e5a3488f222ddf465070b66c8e70e16a
StaciAF <56170981+StaciAF@users.noreply.github.com>
2019-10-04 12:56:30 -0500

    main.c.StaciAF

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 int main()
 {
+  $ git commit -m DONE, son.
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
 }
